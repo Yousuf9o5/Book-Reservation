a41ec47710679edf41cbbb8ee9eda22d2421f504
yousif <yousuf1212121@gmail.com>
2024-03-23 06:08:54 +0300

    [UPDATE] add docs for check token

--- src/middlewares/check.token.js
+++ src/middlewares/check.token.js
@@ -2,11 +2,19 @@ import { error } from "../utils/response.js";
 import { verify } from "jsonwebtoken";
 import dotenv from "dotenv";
 import User from "../../database/schemas/user.schema.js";
+import express from "express";
 
 dotenv.config();
 
 const secretKey = process.env.SECRET_KEY;
 
+/**
+ * Middleware to check and validate JWT token from request headers.
+ * @param {express.Request} req - The request object containing JWT token in the headers.
+ * @param {express.Response} res - The response object to send the result of the token validation.
+ * @param {express.NextFunction} next - The next middleware function in the request-response cycle.
+ * @returns {Promise<void>} A promise representing the asynchronous operation.
+ */
 async function checkToken(req, res, next) {
   try {
     const token = req.headers.authorization?.split(" ")[1];
